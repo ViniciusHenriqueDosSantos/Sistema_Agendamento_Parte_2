8c52ae337483b8f15232c2c126e8524fe6f2a4d3
anabiagarcia <anabia.garcia29@gmail.com>
2026-05-09 00:53:13 -0300

    feat: Classe Reserva

---
+++ src/reservas.py
@@ -0,0 +1,124 @@
+from datetime import date, time
+from enum import Enum
+
+from salas import Sala
+from usuarios import Usuario
+
+
+class StatusReserva(Enum):
+    """
+    Representa os possíveis estados de uma reserva.
+    """
+
+    ATIVA = "Ativa"
+    CANCELADA = "Cancelada"
+    FINALIZADA = "Finalizada"
+
+
+class Reserva:
+    """
+    Representa uma reserva de sala realizada por um usuário.
+
+    Cada reserva possui:
+    - um identificador único;
+    - uma sala;
+    - um usuário responsável;
+    - uma data;
+    - um horário;
+    - um status.
+    """
+
+    proximo_id = 1
+
+    def __init__(self, sala: Sala, usuario: Usuario, data: date, horario: time):
+        """
+        Inicializa uma nova reserva.
+
+        Args:
+            sala (Sala): Sala reservada.
+            usuario (Usuario): Usuário responsável pela reserva.
+            data (date): Data da reserva.
+            horario (time): Horário da reserva.
+        """
+
+        self._id = Reserva.proximo_id
+        self._sala = sala
+        self._usuario = usuario
+        self._data = data
+        self._horario = horario
+        self._status = StatusReserva.ATIVA
+
+        Reserva.proximo_id += 1
+
+    def get_usuario(self) -> Usuario:
+        """
+        Retorna o usuário responsável pela reserva.
+
+        Returns:
+            Usuario: Usuário da reserva.
+        """
+
+        return self._usuario
+
+    def get_sala(self) -> Sala:
+        """
+        Retorna a sala reservada.
+
+        Returns:
+            Sala: Sala da reserva.
+        """
+
+        return self._sala
+
+    def cancelar_reserva(self):
+        """
+        Cancela a reserva.
+
+        Futuramente deverá notificar os observadores.
+        """
+
+        self._status = StatusReserva.CANCELADA
+
+    def set_usuario(self, usuario: Usuario):
+        """
+        Altera o usuário responsável pela reserva.
+
+        Args:
+            usuario (Usuario): Novo usuário.
+        """
+
+        #adicionar observer
+        self._usuario = usuario
+
+    def set_sala(self, sala: Sala):
+        """
+        Altera a sala da reserva.
+
+        Args:
+            sala (Sala): Nova sala.
+        """
+
+        #adicionar observer
+        self._sala = sala
+
+    def set_hora(self, horario: time):
+        """
+        Altera o horário da reserva.
+
+        Args:
+            horario (time): Novo horário.
+        """
+
+        #adicionar observer
+        self._horario = horario
+
+    def set_data(self, data: date):
+        """
+        Altera a data da reserva.
+
+        Args:
+            data (date): Nova data.
+        """
+
+        #adicionar observer
+        self._data = data
